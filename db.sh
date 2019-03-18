8ab66270295841f2ab9db0718f11f760d45478ef
Kerim2102000 <kerimcetin2000@gmail.com>
2019-03-18 09:54:03 +0100

    Update db.sh

--- db.sh
+++ db.sh
@@ -4,8 +4,8 @@
 #
 
 # root Password setzen, damit kein Dialog erscheint und die Installation haengt!
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root4me'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root4me'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password Welcome1'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password Welcome1'
 
 # Installation
 sudo apt-get install -y mysql-server
@@ -14,8 +14,8 @@ sudo apt-get install -y mysql-server
 sudo sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
 # User fuer Remote Zugriff einrichten - aber nur fuer Host web 192.168.10.101
-mysql -uroot -root4me <<%EOF%
-	CREATE USER 'root'@'192.168.10.101' IDENTIFIED BY 'root4me';
+mysql -uroot -Welcome1 <<%EOF%
+	CREATE USER 'root'@'192.168.10.101' IDENTIFIED BY 'admin';
 	GRANT ALL PRIVILEGES ON *.* TO 'root'@'192.168.10.101';
 	FLUSH PRIVILEGES;
 %EOF%
