1f2013156ebb8dc20895138a80aa978ff797a25d
Kerim2102000 <47856019+Kerim2102000@users.noreply.github.com>
2019-03-21 14:31:10 +0100

    Add files via upload

--- db.sh
+++ db.sh
@@ -3,7 +3,8 @@
 #	Datenbank installieren und Konfigurieren
 #
 
-# root Password setzen, damit kein Dialog erscheint und die Installation haengt!
+
+# root Password setzen
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password Welcome1'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password Welcome1'
 
@@ -13,11 +14,14 @@ sudo apt-get install -y mysql-server
 # MySQL Port oeffnen
 sudo sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
-# User fuer Remote Zugriff einrichten - aber nur fuer Host web 192.168.10.101
+
+
+# User fuer Remote Zugriff nur fuer webserver01 192.168.10.101
 mysql -uroot -Welcome1 <<%EOF%
 	CREATE USER 'admin'@'192.168.10.101' IDENTIFIED BY '1234';
 	GRANT ALL PRIVILEGES ON *.* TO 'admin'@'192.168.10.101';
+
 %EOF%
 
-# Restart fuer Konfigurationsaenderung
+
 sudo service mysql restart
